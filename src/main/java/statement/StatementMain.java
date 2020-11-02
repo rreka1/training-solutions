545ea08f88c54dccad54d3e340b3e56cb6019c25
rreka1 <rekareichenbach@gmail.com>
2020-11-02 19:37:59 +0100

    StatementMain (statements)

---
+++ src/main/java/statement/StatementMain.java
@@ -0,0 +1,19 @@
+package statement;
+
+public class StatementMain {
+
+    public static void main(String[] args) {
+
+        int x = 5 + 6;
+
+        int y = 11 - x;
+
+        int z = 8;
+
+        boolean b = x > y;
+
+        boolean c = (b == true) || (z > 5);
+
+        z++;
+    }
+}
