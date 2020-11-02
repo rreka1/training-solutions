14acc1a21a5f89fccae799df13f034b128267913
rreka1 <rekareichenbach@gmail.com>
2020-11-02 20:19:41 +0100

    Time (statements)

---
+++ src/main/java/statement/Time.java
@@ -0,0 +1,31 @@
+package statement;
+
+public class Time {
+
+    private int ora;
+    private int perc;
+    private int masodperc;
+
+    public Time(int ora, int perc, int masodperc) {
+        this.ora = ora;
+        this.perc = perc;
+        this.masodperc = masodperc;
+    }
+
+    public int getInMinutes() {
+        return (ora*60 + perc);
+    }
+
+    public int getInSeconds() {
+        return (ora*3600 + perc*60 + masodperc);
+    }
+
+    public boolean earlierThan(Time time) {
+        return this.getInSeconds() < time.getInSeconds();
+    }
+
+    public String toString() {
+        return ora + ":" + perc + ":" + masodperc;
+    }
+
+}
---
+++ src/main/java/statement/TimeMain.java
@@ -0,0 +1,32 @@
+package statement;
+
+import java.util.Scanner;
+
+public class TimeMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Az első időpont: (óra perc másodperc szóközökkel elválasztva)");
+
+        int ora = scanner.nextInt();
+        int perc = scanner.nextInt();
+        int masodperc = scanner.nextInt();
+
+        Time time1 = new Time(ora, perc, masodperc);
+
+        System.out.println("A második időpont: (óra perc másodperc szóközökkel elválasztva)");
+
+        ora = scanner.nextInt();
+        perc = scanner.nextInt();
+        masodperc = scanner.nextInt();
+
+        Time time2 = new Time(ora, perc, masodperc);
+
+        System.out.println("Az első időpont " + time1.toString() + " = " + time1.getInMinutes() + " perc");
+        System.out.println("A második időpont " + time2.toString() + " = " + time2.getInSeconds() + " másodperc");
+        System.out.println("Az első korábbi, mint a második? " + time1.earlierThan(time2));
+
+    }
+}
