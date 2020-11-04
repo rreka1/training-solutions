5775f4248431ea8a6dd4226a290fb0e597560192
rreka1 <rekareichenbach@gmail.com>
2020-11-04 22:24:42 +0100

    Books (arraylist)

---
+++ src/main/java/arraylist/Books.java
@@ -0,0 +1,51 @@
+package arraylist;
+
+import classstructureconstructors.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    private List<String> books = new ArrayList<>();
+
+    public void add(String book) {
+
+        books.add(book);
+    }
+
+    public List<String> findAllByPrefix(String prefix) {
+
+        List<String> samePrefix = new ArrayList<>();
+
+        for (String book: books) {
+
+            if(book.indexOf(prefix, 0) == 0) {
+
+                samePrefix.add(book);
+            }
+        }
+        return samePrefix;
+    }
+
+    public List<String> getTitles() {
+
+        return books;
+    }
+
+    //...
+    public static void main(String[] args) {
+
+        Books books = new Books();
+
+        books.add("Antigoné");
+        books.add("Egri csillagok");
+        books.add("A kis herceg");
+        books.add("Egri csillagok");
+
+        System.out.println(books.getTitles());
+
+        System.out.println(books.findAllByPrefix("Egri"));
+
+    }
+}
