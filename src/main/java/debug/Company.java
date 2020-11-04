38c3499f42d2671d9eeb45b93fd1eb0fdf5e8e8c
rreka1 <rekareichenbach@gmail.com>
2020-11-04 23:10:18 +0100

    Company & Employee (debug)

---
+++ src/main/java/debug/Company.java
@@ -0,0 +1,41 @@
+package debug;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public Company(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+
+        employees.add(employee);
+    }
+
+    public Employee findEmployeeByName(String name) {
+
+        for (Employee employee : employees) {
+
+            if(name.equals(employee.getName())) {
+
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    public List<String> listEmployeeNames() {
+
+        List<String> names = new ArrayList<>();
+
+        for (Employee employee : employees) {
+
+            names.add(employee.getName());
+        }
+        return names;
+    }
+}
