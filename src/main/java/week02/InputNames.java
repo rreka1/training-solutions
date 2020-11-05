3048ff8ba6dfb238aa089c20af2741cfbc4fe3db
rreka1 <rekareichenbach@gmail.com>
2020-11-05 14:14:15 +0100

    week02 InputNames

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,29 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] nevek = new String[5];
+
+        for (int i = 0; i < nevek.length; i++) {
+
+            nevek[i] = scanner.nextLine();
+
+        }
+
+        for (int i = 0; i < nevek.length; i++) {
+
+            System.out.println((i+1) + " .elem: " + nevek[i]);
+
+        }
+
+
+
+
+    }
+}
