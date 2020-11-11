45b1db0072d6cce7a8a994f301047e6a114d0aa8
rreka1 <rekareichenbach@gmail.com>
2020-11-11 17:32:18 +0100

    Person & Address (attributes)

---
+++ src/main/java/attributes/person/Person.java
@@ -0,0 +1,38 @@
+package attributes.person;
+
+public class Person {
+
+    private String name;
+    private String identificationCard;
+    private Address address;
+
+    public Person(String name, String identificationCard) {
+        this.name = name;
+        this.identificationCard = identificationCard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIdentificationCard() {
+        return identificationCard;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public String personToString() {
+        return "(Person) Name: " + getName() + ", IdentificationCard: " + getIdentificationCard();
+    }
+
+    public void correctData(String name, String identificationCard) {
+        this.name = name;
+        this.identificationCard = identificationCard;
+    }
+
+    public void moveTo(Address address) {
+        this.address = address;
+    }
+}
