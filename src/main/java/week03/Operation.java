a446ef2e2b820556ec3909ed31489d8d7ccff0ea
rreka1 <rekareichenbach@gmail.com>
2020-11-13 13:43:13 +0100

    Operation

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,28 @@
+package week03;
+
+import operators.Operators;
+
+public class Operation {
+
+    private int leftValue;
+    private int rightValue;
+
+    public Operation(String string) {
+
+        String left = string.substring(0, string.indexOf("+"));
+        String right = string.substring(string.indexOf("+") + 1);
+
+        this.leftValue = Integer.parseInt(left);
+        this.rightValue = Integer.parseInt(right);
+    }
+
+    public int getResult() {
+        return leftValue + rightValue;
+    }
+
+    public static void main(String[] args) {
+
+        Operation operation = new Operation("161+18");
+        System.out.println(operation.getResult());
+    }
+}
