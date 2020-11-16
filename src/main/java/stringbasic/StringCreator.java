043c7d5acf1dea8e9d7645a1d4f749403d00a8a3
rreka1 <rekareichenbach@gmail.com>
2020-11-16 17:37:17 +0100

    StringCreator & StringCreatorTest (stringbasic)

---
+++ src/main/java/stringbasic/StringCreator.java
@@ -0,0 +1,12 @@
+package stringbasic;
+
+public class StringCreator {
+
+    public String createStringForHeap() {
+        return new String("string");
+    }
+
+    public String createStringForPool() {
+        return "string";
+    }
+}
