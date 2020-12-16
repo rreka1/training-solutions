8cb5a5d187f1299a69282111243997a7ecab94ee
rreka1 <rekareichenbach@gmail.com>
2020-12-16 23:19:17 +0100

    TypeWriterState & Test (statemachine.typewriter)

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,19 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+
+    LOWERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    },
+    UPPERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}
