0c3a9af2c0d481d8989a026789fbcd3e986c85de
rreka1 <rekareichenbach@gmail.com>
2021-01-08 19:12:57 +0100

    week10d05 Calculator /mod/

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -1,74 +1,59 @@
 package week10d05;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
+
+    private static final int MAX_NUMBER_FOR_SUM = 4;
 
     //junior-mid
     public int findMinSum(int[] arr) {
         int sum = 0;
         Arrays.sort(arr);
-        for (int i = 0; i < numbersToSum(arr.length); i++) {
+        for (int i = 0; i < MAX_NUMBER_FOR_SUM && i < arr.length; i++) {
             sum += arr[i];
         }
-        System.out.println(sum);
+        System.out.println("A legkisebbek összege: " + sum);
         return sum;
-    }
-
-    private int numbersToSum(int arrLength) {
-        int numbersToSum = 0;
-        if (arrLength >= 4) {
-            numbersToSum = 4;
-        } else {
-            numbersToSum = arrLength;
-        }
-        return numbersToSum;
     }
 
 
     //senior
-    public MinMaxSum findMinMaxSum(int[] arr) {
+    public void findMinMaxSum(int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Array is null!");
+        }
         int minSum = 0;
         int maxSum = 0;
         Arrays.sort(arr);
-        for (int i = 0; i < numbersToSum(arr.length); i++) {
+        for (int i = 0; i < Math.min(MAX_NUMBER_FOR_SUM, arr.length); i++) {
             minSum += arr[i];
             maxSum += arr[arr.length - 1 - i];
         }
-        System.out.println(minSum);
-        System.out.println(maxSum);
-        return new MinMaxSum(minSum, maxSum);
+        System.out.printf("minSum: %d%nmaxSum: %d%n", minSum, maxSum);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Tömb elemszáma:");
-        int arrLength = scanner.nextInt();
-        int[] arr = new int[arrLength];
-        System.out.println("Tömb elemei:");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
+        List<Integer> numbers = new ArrayList<>();
+        String s = scanner.nextLine();
+        while (!s.isBlank()) {
+            try {
+                numbers.add(Integer.parseInt(s));
+            } catch (NumberFormatException nfe) {
+                System.err.println("Számot adj meg!");
+            }
+            s = scanner.nextLine();
+        }
+        int[] arr = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            arr[i] = numbers.get(i);
         }
         new Calculator().findMinMaxSum(arr);
     }
 }
 
-class MinMaxSum {
-    private int minSum;
-    private int maxSum;
-
-    public MinMaxSum(int minSum, int maxSum) {
-        this.minSum = minSum;
-        this.maxSum = maxSum;
-    }
-
-    public int getMinSum() {
-        return minSum;
-    }
-
-    public int getMaxSum() {
-        return maxSum;
-    }
-}
 
--- src/test/java/week10d05/CalculatorTest.java
+++ src/test/java/week10d05/CalculatorTest.java
@@ -15,15 +15,4 @@ class CalculatorTest {
         assertEquals(17, calculator.findMinSum(new int[]{9, 7, 1}));
         assertEquals(0, calculator.findMinSum(new int[]{}));
     }
-
-    @Test
-    void findMinMaxSum() {
-        Calculator calculator = new Calculator();
-        assertEquals(16, calculator.findMinMaxSum(new int[]{9, 7, 1, 5, 3}).getMinSum());
-        assertEquals(24, calculator.findMinMaxSum(new int[]{9, 7, 1, 5, 3}).getMaxSum());
-        assertEquals(17, calculator.findMinMaxSum(new int[]{9, 7, 1}).getMinSum());
-        assertEquals(17, calculator.findMinMaxSum(new int[]{9, 7, 1}).getMaxSum());
-        assertEquals(0, calculator.findMinMaxSum(new int[]{}).getMinSum());
-        assertEquals(0, calculator.findMinMaxSum(new int[]{}).getMaxSum());
-    }
 }
