2e79c1f5791a8803e10f45ad05746eb634e3de57
rreka1 <rekareichenbach@gmail.com>
2021-01-19 11:28:44 +0100

    week03d04 Phone & PhoneParser

---
+++ src/main/java/week03/Phone.java
@@ -0,0 +1,34 @@
+package week03;
+
+/*
+ * Írj egy Phone oszályt, melynek van egy prefix és egy number String attribútuma!
+ * Írj egy PhoneParser osztályt egy parse(String) metódussal, ami beolvas egy 10-1234567 formátumú szöveget,
+ * és visszaad egy Phone objektumot, feltöltve a prefix értékét 10, a number értékét 1234567 értékekkel!
+ * */
+
+public class Phone {
+
+    private String prefix;
+    private String number;
+
+    public Phone(String prefix, String number) {
+        this.prefix = prefix;
+        this.number = number;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "prefix='" + prefix + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/week03/PhoneParser.java
@@ -0,0 +1,26 @@
+package week03;
+
+import java.util.Scanner;
+
+public class PhoneParser {
+
+    public Phone parsePhone(String phoneNumber) {
+        String prefix = phoneNumber.substring(0, phoneNumber.indexOf("-"));
+        String number = phoneNumber.substring(phoneNumber.indexOf("-") + 1);
+        Phone phone = new Phone(prefix, number);
+        return phone;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        PhoneParser parser = new PhoneParser();
+
+        System.out.println("Add meg a telefonszámot a következő formátumban: 10-1234567");
+        Phone phone = parser.parsePhone(scanner.nextLine());
+
+        System.out.println("prefix: " + phone.getPrefix());
+        System.out.println("number: " + phone.getNumber());
+        System.out.println(phone);
+    }
+}
