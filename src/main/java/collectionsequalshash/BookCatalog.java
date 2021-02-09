946e1037a60ce5d2946def76979d3c25036618d5
rreka1 <rekareichenbach@gmail.com>
2021-02-09 13:56:30 +0100

    BookCatalog & Book (collectionsequalshash)

---
+++ src/main/java/collectionsequalshash/BookCatalog.java
@@ -0,0 +1,31 @@
+package collectionsequalshash;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BookCatalog {
+
+    public Book findBookByTitleAuthor(List<Book> books, String title, String author) {
+        Book book = new Book(title, author);
+        if (!books.contains(book)) {
+            return null;
+        }
+        return books.get(books.indexOf(book));
+    }
+
+    public Book findBook(List<Book> books, Book book) {
+        if (!books.contains(book)) {
+            return null;
+        }
+        return books.get(books.indexOf(book));
+    }
+
+    public Set<Book> addBooksToSet(Book[] books) {
+        Set<Book> bookSet = new HashSet<>();
+        for (Book book : books) {
+            bookSet.add(book);
+        }
+        return bookSet;
+    }
+}
