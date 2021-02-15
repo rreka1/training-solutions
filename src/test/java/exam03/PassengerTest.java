1dd60ef7d5aef2c125d9e98559344874088f2908
rreka1 <rekareichenbach@gmail.com>
2021-02-15 18:28:02 +0100

    exam03 Tests

---
+++ src/test/java/exam03/PassengerTest.java
@@ -0,0 +1,13 @@
+package exam03;
+
+import org.junit.jupiter.api.Test;
+
+public class PassengerTest {
+
+    @Test
+    void create() {
+        Passenger passenger = new Passenger("John Doe", CruiseClass.LUXURY);
+        assertEquals("John Doe", passenger.getName());
+        assertEquals(CruiseClass.LUXURY, passenger.getCruiseClass());
+    }
+}
