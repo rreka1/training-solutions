8a17f660f70ab16d89d5d55c0ad4cb8233af6877
rreka1 <rekareichenbach@gmail.com>
2021-05-31 09:29:42 +0200

    Az Optional osztály - Közösségi háló

---
+++ src/main/java/lambdaoptional/SocialNetwork.java
@@ -0,0 +1,35 @@
+package lambdaoptional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class SocialNetwork {
+
+    private final List<Member> members;
+
+    public SocialNetwork(List<Member> members) {
+        this.members = members;
+    }
+
+    public Optional<Member> findFirst(Predicate<Member> condition) {
+        for (Member member : members) {
+            if (condition.test(member)) {
+                return Optional.of(member);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Double> averageNumberOfSkills() {
+        if (members.isEmpty()) {
+            return Optional.empty();
+        }
+        int sum = 0;
+        for (Member member : members) {
+            sum += member.getSkills().size();
+        }
+        return Optional.of((double) sum / members.size());
+    }
+}
+
